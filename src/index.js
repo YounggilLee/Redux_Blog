b5c4eb56b7b206ea7cc18e5b7b15f8854c79fe1f
Younggil Lee <leyoungg@sheridancollege.ca>
2017-09-14 13:39:49 -0400

    Install react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
 import './index.css';
-import App from './components/app';
+import { Router, browserHistory } from 'react-router';
+//import App from "./components/app";
+import reducers from "./reducers";
+import routes from './routes';
 
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+  
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>,
+  document.getElementById('root')
+);
+
 
