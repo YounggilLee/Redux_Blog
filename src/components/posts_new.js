a64069c481f32a87636345390c3bc151538737ab
Younggil Lee <leyoungg@sheridancollege.ca>
2017-09-14 16:44:57 -0400

    Add route to move to a create post page

---
+++ src/components/posts_new.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+
+        return (
+            <div> Create From</div>
+        );
+    }
+}
+
+export default PostsNew;
